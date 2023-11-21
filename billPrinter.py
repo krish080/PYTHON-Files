b8780b8991345f5f641dde22655a8d7a594c0136
krish080 <89718779+krish080@users.noreply.github.com>
2023-11-21 16:07:57 +0530

    Update billPrinter.py

--- billPrinter.py
+++ billPrinter.py
@@ -14,6 +14,7 @@ dt=datetime.now()
 dt=str(dt)
 dt=dt.split()
 
+#getting the item name price and quantity
 for i in range(noOfItem):
     os.system('cls')
     itemName.append(input("Enter the name of item no "+str(i+1)+": "))
@@ -24,6 +25,7 @@ for i in range(noOfItem):
     os.system('cls')
     itemTotal.append(itemPrice[i]*itemQuantity[i])
 
+#printing the bill
 print(f"{'Welcome to Target': ^42}")
 print("------------------------------------------")
 print("Date :"+str(dt[0]))
@@ -35,10 +37,11 @@ print(" ")
 for i in range(noOfItem):
     print(f"{str(itemName[i]): <12}",f"{str(itemPrice[i]): <11}",f"{str(itemQuantity[i]): <9}",f"{str(itemTotal[i]): <10}")
 
+#finding the total amount
 tot=0
 for i in itemTotal:
     tot+=i
-
+#calculating tax
 gst=(tot/100)*2.5
 cgst=(tot/100)*6
 
@@ -49,6 +52,7 @@ print("------------------------------------------")
 tot+=gst
 tot+=cgst
 
+#printing tax and total amount
 print("------------------------------------------")
 print(f"{'GST:'+str(gst): >42}")
 print(f"{'CGST:'+str(cgst): >42}")
